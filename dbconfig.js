37f0e8887db39b939a9d841392d1df5b7f9ce397
edwardp22 <58954908+edwardp22@users.noreply.github.com>
2022-01-16 06:11:20 -0800

    End points for invoices and clients

---
+++ dbconfig.js
@@ -0,0 +1,11 @@
+const config = {
+  user: 'readonlyuser',
+  password: 'TestUser123*',
+  server: 'edwardpena.database.windows.net',
+  database: 'test',
+  options: {
+    encrypt: true
+  }
+}
+  
+module.exports = config;
---
+++ routes/invoices.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const config = require('../dbconfig');
+const sql = require('mssql');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const pool = await sql.connect(config);
+    const clients = await pool.request().query("select * from ViewInvoices");
+    res.json(clients.recordsets[0]);
+  }
+  catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
